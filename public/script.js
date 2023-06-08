fab758da9f4643538fcb7543d6738b630c77bcef
Erik Knoef <erikknoef1997@hotmail.com>
2023-06-08 18:50:18 +0100

    viable

---
+++ public/script.js
@@ -0,0 +1,6 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+// Add an event listener to the 'DOMContentLoaded' event
+document.addEventListener('DOMContentLoaded', function() {
+  // Scroll to the top of the page
+  window.scrollTo(0, 0);
+});
--- src/Pages/Homepage/index.jsx
+++ src/Pages/Homepage/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
 export default function HomePage() {
+  useEffect(() => {
+    // Disable scrolling on mount
+    document.body.style.overflow = "hidden";
+
+    // Re-enable scrolling on unmount
+    return () => {
+      document.body.style.overflow = "visible";
+    };
+  }, []);
   return (
-    <div>
-      <h1>HomePage</h1>
+    <div className='homepage disable-scroll'>
     </div>
   )
 }
